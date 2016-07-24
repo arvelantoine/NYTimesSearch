1fc9bf6532e354acb8f41c2e18e5c814daf1889f
Arvel Antoine <arvelantoine@yahoo.fr>
2016-07-25 02:22:53 +1200

    NYTimesSearch data

---
+++ app/src/main/java/com/example/evanscadet/nytimessearch/activities/SearchActivity.java
@@ -0,0 +1,169 @@
+package com.example.evanscadet.nytimessearch.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.evanscadet.nytimessearch.Article;
+import com.example.evanscadet.nytimessearch.ArticleArrayAdapter;
+import com.example.evanscadet.nytimessearch.R;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+import java.util.ArrayList;
+
+
+public class SearchActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    GridView gvResults;
+    Button btnSearch;
+
+    ArrayList<Article> articles;
+    ArticleArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupViews();
+
+
+    }
+
+    public void  setupViews(){
+        etQuery = (EditText) findViewById(R.id.etQuery);
+        gvResults = (GridView) findViewById(R.id.gvResults);
+        btnSearch = (Button) findViewById(R.id.btnSearch);
+        articles = new ArrayList<>();
+        adapter = new ArticleArrayAdapter(this, articles);
+        gvResults.setAdapter(adapter);
+
+        // hook up listener for grid click
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+              // create an intent to display the article
+                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+                // get the article to display
+                Article article = articles.get(position);
+                // pass in the article into intent
+                i.putExtra("article", article);
+                // launch the activity
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+
+                return true;
+    }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            launchSetting();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+
+       // Toast.makeText(this, "searching for" + query, Toast.LENGTH_LONG).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params = new RequestParams();
+        params.put("api-key","18c37b35082e47db8898430b91251403");
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                JSONArray articleJsonResults = null;
+
+                try {
+                    articleJsonResults = response.getJSONObject("response").getJSONArray("docs");
+                    adapter.addAll(Article.fromJSONArray(articleJsonResults));
+                    Log.d("DEBUG", articleJsonResults.toString());
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void launchSetting(){
+        Intent i = new Intent(this, SettingActivity.class);
+        startActivity(i);
+
+    }
+}
---
+++ app/src/main/java/com/example/evanscadet/nytimessearch/activities/SettingActivity.java
@@ -0,0 +1,84 @@
+package com.example.evanscadet.nytimessearch.activities;
+
+import android.app.DatePickerDialog;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.example.evanscadet.nytimessearch.DatePickerFragment;
+import com.example.evanscadet.nytimessearch.R;
+
+import java.text.SimpleDateFormat;
+
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class SettingActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        spinner();
+
+
+    }
+
+    public void onSave(View view) {
+        finish();
+    }
+
+    public void onShowDatePicker(View view) {
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        // store the values selected into a Calendar instance
+        final Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, monthOfYear);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        SimpleDateFormat formatDate = new SimpleDateFormat("yy-MM-dd");
+
+        EditText beginDate = (EditText) findViewById(R.id.begindate);
+
+        beginDate.setText(formatDate.format(c.getTime()));
+
+    }
+
+    public Spinner spinner() {
+        //fetch the spinner in the xml file
+        spinner = (Spinner) findViewById(R.id.spinner);
+        //Create the arrayList to put elemet in the spinner
+        List exempleList = new ArrayList();
+        exempleList.add("Oldest");
+        exempleList.add("Newest");
+
+        //use an adapter to bind data
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, exempleList);
+
+
+        // set the dropdown
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Enfin on passe l'adapter au Spinner et c'est tout
+        spinner.setAdapter(adapter);
+
+        return spinner;
+    }
+
+}
